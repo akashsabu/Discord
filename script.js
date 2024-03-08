abace1f99c09c37528fabd6726eb1e83afe234f6
akashsabu <akashpaulmsabu1999@gmail.com>
2024-03-08 19:18:00 +0530

    responsive in html and css done

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('open');
+    document.querySelector('.nav-menu ul').classList.toggle('flex');
+});
