a00ecd87fb2ed6ad13746280a70d2461a52e696b
akashsabu <akashpaulmsabu1999@gmail.com>
2024-03-11 19:23:23 +0530

    corrections completed

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".hamburger-button").click(function () {
+    $("#nav").toggleClass("active");
+    $("#nav ul").toggleClass("flex");
+    $(".nav-close").css("display", "block");
+    $("#overlay").toggleClass("overlay");
+  });
 
-    $(".hamburger-button").click(function () {
-        $("#nav").toggleClass("active");
-        $("#nav ul").toggleClass("flex");
-        $("#overlay").toggleClass("overlay");
-    });
+  $(".nav-close").click(function () {
+    $("#nav").toggleClass("active");
+    $("#nav ul").toggleClass("flex");
+    $("#overlay").toggleClass("overlay");
+    $(".nav-close").css("display", "none");
+  });
+  $("#overlay").click(function () {
+    $("#nav").toggleClass("active");
+    $("#nav ul").toggleClass("flex");
+    $(".nav-close").css("display", "none");
+    $("#overlay").toggleClass("overlay");
+  });
 
-    
-    $('#overlay').click(function () {
-        $("#nav").toggleClass("active");
-        $("#nav ul").toggleClass("flex");
-        $("#overlay").toggleClass("overlay");
-    });
+  mQBasedClass(screen.width);
 
-
+  $(window).resize(function () {
     mQBasedClass(screen.width);
-
-    $(window).resize(function() {
-        mQBasedClass(screen.width);
-    });
-    function mQBasedClass(width){
-        if(width<981) {
-            $("#nav").addClass('nav-links');
-        }
-        else{
-            $("#nav").removeClass('nav-links');
-
-        }
-    } 
+  });
+  function mQBasedClass(width) {
+    if (width < 981) {
+      $("#nav").addClass("nav-links");
+    } else {
+      $("#nav").removeClass("nav-links");
+    }
+  }
 });
-
-
